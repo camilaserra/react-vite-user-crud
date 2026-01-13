be5014175b81b5eb3a6c7390f424942c120e418e
Camila Serra <camilaserrasilva@gmail.com>
2026-01-13 20:19:28 -0300

    global footer added

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import './styles/variables.css'
 import './styles/global.css'
 
 import App from './App.jsx'
-
+import Footer from './components/Footer/index.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Footer />
   </StrictMode>,
 )
