76601e773683bb34ae35ad14a686d604e8decd82
Camila Serra <camilaserrasilva@gmail.com>
2026-01-13 20:17:38 -0300

    footer implemented

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <footer className={styles.footer}>
+      <p>
+        Teste Camila Serra -  © {currentYear}
+      </p>
+    </footer>
+  )
+}
