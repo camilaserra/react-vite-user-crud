60ffff44e088a47706319690864cc60acc90587d
Camila Serra <camilaserrasilva@gmail.com>
2026-01-13 17:18:32 -0300

    Input masks moved for global use

---
+++ src/utils/inputMasks.jsx
@@ -0,0 +1,19 @@
+export function cpfMask(value = '') {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})$/, '$1-$2')
+}
+
+export function phoneMask(value = '') {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d)/, '$1-$2')
+}
+
+export function emailMask(value) {
+  return value
+    .replace(/\s/g, '')
+}
