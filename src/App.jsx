0477ed1c7f61ca706cf0adb7d841844ba73c92d1
Camila Serra <camilaserrasilva@gmail.com>
2026-01-13 17:23:50 -0300

    renaming paths and adjusting height

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { fetchUsers } from './services/usersApi'
 import { getUsers, saveUsers } from './services/localStorageService'
-import FormPage from './pages/FormPage'
-import ListPage from './pages/ListPage'
+import FormPage from './pages/home/FormPage'
+import ListPage from './pages/listPage/ListPage'
 
 function App() {
   useEffect(() => {
